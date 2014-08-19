9c7170921233a620838edda00464ca613c31fa56
Cosmin Ioan <cioan@pih.org>
2014-08-19 10:16:53 -0400

    add pih_java module

---
+++ modules/pih_java/manifests/init.pp
@@ -0,0 +1,12 @@
+class pih_java {
+    
+
+    $oracle_java_installer = 'oracle-java6-installer_6u45-0~webupd8~8_all.deb'
+    $tmp_oracle_java_installer = '/tmp/${oracle_java_installer}'
+		
+	file { $tmp_oracle_java_installer:
+		ensure  => file,
+		source	=> "puppet:///modules/pih_java/${oracle_java_installer}",		
+	}
+
+}
