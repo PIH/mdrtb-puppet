d62479bf1817eed7d7e8cb0ef8132ca3a3b30036
Cosmin Ioan <cioan@pih.org>
2014-08-19 14:04:24 -0400

    correct java installer deb package name

--- modules/pih_java/manifests/init.pp
+++ modules/pih_java/manifests/init.pp
@@ -2,7 +2,7 @@ class pih_java {
     
 
     $oracle_java_installer = "oracle-java6-installer_6u45-0.deb"
-    $tmp_oracle_java_installer = "/tmp/${oracle_java_installer}""
+    $tmp_oracle_java_installer = "/tmp/${oracle_java_installer}"
 		
 	file { $tmp_oracle_java_installer:
 		ensure  => file,
