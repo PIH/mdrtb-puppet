d2277a2724917b9347ab603d4b057e610399586a
Cosmin Ioan <cioan@pih.org>
2014-08-21 14:59:49 -0400

    add mysql initial install script

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ puppet apply -v \
   --detailed-exitcodes \
   --logdest=console \
   --logdest=syslog \
+  --hiera_config=./hiera.yaml \
   --modulepath=./modules \
   manifests/site.pp
---
+++ modules/pih_mysql/manifests/init.pp
@@ -0,0 +1,38 @@
+class pih_mysql {
+    
+	$mysql_sys_user_group = hiera('mysql_sys_user_group')
+	$mysql_sys_user_name = hiera('mysql_sys_user_name')
+
+	$mysql_home = "/usr/local/mysql"
+	$mysql_tar = "mysql-5.6.20.tar.gz"
+	$mysql_dest = "/usr/local/${mysql_tar}"
+
+	group { $mysql_sys_user_group:
+		ensure => "present",
+	} ->
+
+	user { $mysql_sys_user_name:
+		ensure	=> 'present',
+		gid		=> $mysql_sys_user_group,
+	} ->
+
+	file { $mysql_home:
+		ensure  => directory,
+		owner	=> $mysql_sys_user_name,
+		group 	=> mysql_sys_user_group,
+		recurse	=> true,
+	} -> 
+	
+	file { $mysql_dest:
+		ensure  => file,
+		source	=> "puppet:///modules/pih_mysql/${mysql_tar}",		
+		mode    => '0755',
+	} -> 
+
+	exec { 'unzip-mysql':
+		cwd     => '/usr/local',
+		command => "tar -xzf ${mysql_dest} -C ${mysql_home}",		
+		timeout	=> 0, 
+	}
+
+}
