cbbfd576c1b2c6b7c5f3190c21874c2016267343
Cosmin Ioan <cioan@pih.org>
2014-09-19 14:30:54 -0400

    download keys from the amigo server

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-  if [ ! -f /etc/ssl/private/pih-emr.org.key ]
+  if [ ! -f /etc/ssl/private/pih-emr.org.key ] || [ ! -f /etc/puppet/keys/id_rsa ] 
   then
     echo "Please provide a username to fetch private data"
     read user
@@ -8,6 +8,7 @@
     scp $user@72.249.183.205:/home/backups/.keys/* .
 
     mv pih-emr.org.key /etc/ssl/private/
+    mv id_rsa /etc/puppet/keys/
   fi
 
 
